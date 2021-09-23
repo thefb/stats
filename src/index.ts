3114ba1facf2702f62601e32439876dd15528ac4
Fabiano Siqueira <fabiano.siqueirab@gmail.com>
2021-09-23 01:15:48 -0300

    add summary

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import { WinsAnalysis } from './analyzers/WinsAnalysis'
 import { CsvFileReader } from './CsvFileReader'
 import { MatchReader } from './MatchReader'
+import { ConsoleReport } from './reportTargets/ConsoleReport'
+import { Summary } from './Summary'
 
 // Create an object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader('football.csv')
@@ -10,3 +13,6 @@ const matchReader = new MatchReader(csvFileReader)
 matchReader.load()
 
 // matchReader.matches
+const winsAnalysis = new WinsAnalysis('Arsenal')
+
+new Summary(winsAnalysis, new ConsoleReport())
